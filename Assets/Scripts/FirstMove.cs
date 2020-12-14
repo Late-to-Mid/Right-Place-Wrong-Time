ad95336fa0b59be388a9cf4487691dacd7a56f9e
Jesse Sheeler <jessasheeler@gmail.com>
2020-12-14 03:59:38 -0500

    Added temporary script
    Just fiddled around in Unity and got a terrible move script just to test if I can push things

---
+++ Assets/Scripts/FirstMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstMove : MonoBehaviour
+{
+    //Feel free to delete all this code its garbage anyway
+    Vector3 upVector;
+    Vector3 leftVector;
+    Vector3 rightVector;
+    Vector3 forVector;
+    Vector3 backVector;
+    Rigidbody m_Rigidbody;
+
+    public float playerSpeed = 2.0f;
+
+    private void Start()
+    {
+        upVector = new Vector3(0.0f, 1.0f, 0.0f);
+        forVector = new Vector3(0.0f, 0.0f, 1.0f);
+        backVector = new Vector3(0.0f, 0.0f, -1.0f);
+        leftVector = new Vector3(-1.0f, 0.0f, 0.0f);
+        rightVector = new Vector3(1.0f, 0.0f, 0.0f);
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Rigidbody.velocity = upVector * playerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_Rigidbody.velocity = leftVector * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_Rigidbody.velocity = rightVector * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Rigidbody.velocity = forVector * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_Rigidbody.velocity = backVector * playerSpeed;
+        }
+
+    }
+}
