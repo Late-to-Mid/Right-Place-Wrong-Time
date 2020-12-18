8ec2d02ab9ea2d567157fb22cbb8d77a61442fac
Barrett <rossbarr@msu.edu>
2020-12-18 16:57:25 -0500

    Switch back to 3rd person camera. Need gravity and basic character movement again, though

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -4,109 +4,52 @@ using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour
 {
-    public Camera playerCamera;
-    public float playerSpeed = 20.0f;
-
-    [Tooltip("Sensitivity multiplier for moving the camera around")]
-    public float lookSensitivity = 1f;
-
-    [Header("Rotation")]
-    [Tooltip("Rotation speed for moving the camera")]
-    public float rotationSpeed = 200f;
-
-    [Tooltip("Used to flip the vertical input axis")]
-    public bool invertYAxis = false;
-    [Tooltip("Used to flip the horizontal input axis")]
-    public bool invertXAxis = false;
-
-    float m_CameraVerticalAngle = 0f;
+    Camera playerCamera;
+    public float playerSpeed = 10.0f;
 
     // Declare variables for motion vector
-    Rigidbody m_Rigidbody;
     Vector3 motion;
 
     // Declare variables for input
     float horizontalInput;
     float verticalInput;
 
+    // Declare controller
+    public CharacterController controller;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
     void Start()
     {
-        // Get rigidbody component of the object this script is attached to
-        m_Rigidbody = GetComponent<Rigidbody>();
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        playerCamera = Camera.main;
     }
 
     void Update()
     {
         // Call movement method
         Movement();
-
-        HandleCameraControl();
     }
 
     // Get movement input from player
     void Movement()
     {
-        motion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.Translate(motion * playerSpeed * Time.deltaTime);
-    }
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-    public float GetLookInputsHorizontal()
-    {
-        if (invertXAxis)
+        motion = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        if (motion.magnitude >= 0.1f)
         {
-            return -GetMouseLookAxis(GameConstants.k_MouseAxisNameHorizontal);
-        } else
-        {
-            return GetMouseLookAxis(GameConstants.k_MouseAxisNameHorizontal);
-        }
-    }
+            float targetAngle = Mathf.Atan2(motion.x, motion.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-    public float GetLookInputsVertical()
-    {
-        if (invertYAxis)
-        {
-            return -GetMouseLookAxis(GameConstants.k_MouseAxisNameVertical);
-        } else
-        {
-            return GetMouseLookAxis(GameConstants.k_MouseAxisNameVertical);
-        }
-    }
-
-    float GetMouseLookAxis(string mouseInputName)
-    {
-        // Check if this look input is coming from the mouse
-        float i = Input.GetAxisRaw(mouseInputName);
-
-        // apply sensitivity multiplier
-        i *= lookSensitivity;
-
-        // reduce mouse input amount to be equivalent to stick movement
-        i *= 0.01f;
-
-        return i;
-    }
-
-    void HandleCameraControl()
-    {
-        // horizontal character rotation
-        {
-            // rotate the transform with the input speed around its local Y axis
-            transform.Rotate(new Vector3(0f, (GetLookInputsHorizontal() * rotationSpeed), 0f), Space.Self);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
         }
 
-        // vertical camera rotation
-        {
-            // add vertical inputs to the camera's vertical angle
-            m_CameraVerticalAngle += GetLookInputsVertical() * rotationSpeed;
-
-            // limit the camera's vertical angle to min/max
-            m_CameraVerticalAngle = Mathf.Clamp(m_CameraVerticalAngle, -89f, 89f);
-
-            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
-            playerCamera.transform.localEulerAngles = new Vector3(-m_CameraVerticalAngle, 0, 0);
-        }
     }
 }
