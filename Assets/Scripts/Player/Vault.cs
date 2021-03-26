b012e23b2fdf772f87103b6264220e8bf4bb8a35
Jsheeler <jessasheeler@gmail.com>
2021-03-25 21:53:27 -0400

    Added trigger/collision to vault wall
    Added trigger and collision to vault wall to start vault code

---
+++ Assets/Scripts/Player/Vault.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vault : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        //This class is to check if player is colliding with vaultable walls
+
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Mount"))
+        {
+            Debug.Log("Collided with vault wall");
+        }
+
+
+    }
+}
