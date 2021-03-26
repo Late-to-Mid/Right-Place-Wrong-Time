b5aaf07a317b3c34290eac901a1f2fd04a08dd66
Jsheeler <jessasheeler@gmail.com>
2021-03-25 23:36:15 -0400

    Got conditions working for vault
    Helped from barrett ;)

--- Assets/Scripts/Player/Vault.cs
+++ Assets/Scripts/Player/Vault.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Vault : MonoBehaviour
 {
+
+    public bool inCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //inCollider is already true or false
+        if (Input.GetKeyDown(KeyCode.E) && inCollider)
+        {
+            Debug.Log("Vault key pressed");
+        }
 
 
+
+      
     }
 
 
@@ -26,8 +35,16 @@ public class Vault : MonoBehaviour
         if (collider.gameObject.layer == LayerMask.NameToLayer("Mount"))
         {
             Debug.Log("Collided with vault wall");
+
+            inCollider = true;
         }
-
-
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        inCollider = false;
+    }
+
+
 }
+
