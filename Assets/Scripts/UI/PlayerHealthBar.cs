46354cbccb40239160e7b81614ec05470353a343
Rossbarr <Rossbarr@msu.edu>
2021-04-06 13:30:21 -0400

    Add files and prefabs for UI

---
+++ Assets/Scripts/UI/PlayerHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    [Tooltip("Image component dispplaying current health")]
+    public Image healthFillImage;
+
+    Health m_PlayerHealth;
+
+    private void Start()
+    {
+        PlayerCharacterController playerCharacterController = GameObject.FindObjectOfType<PlayerCharacterController>();
+
+        m_PlayerHealth = playerCharacterController.GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        // update health bar value
+        healthFillImage.fillAmount = m_PlayerHealth.currentHealth / m_PlayerHealth.maxHealth;
+    }
+}
