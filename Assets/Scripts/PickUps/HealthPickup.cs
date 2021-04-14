a08637a786230bf079f7a59815b22616552a97a7
Barrett Ross <barrettross23@gmail.com>
2021-04-14 19:46:25 -0400

    Health Pick UP

---
+++ Assets/Scripts/PickUps/HealthPickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [Header("Parameters")]
+    [Tooltip("Amount of health to heal on pickup")]
+    public float healAmount;
+
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+        Health playerHealth = player.GetComponent<Health>();
+        if (playerHealth && playerHealth.canPickup())
+        {
+            playerHealth.Heal(healAmount);
+
+            m_Pickup.PlayPickupFeedback();
+
+            Destroy(gameObject);
+        }
+    }
+}
