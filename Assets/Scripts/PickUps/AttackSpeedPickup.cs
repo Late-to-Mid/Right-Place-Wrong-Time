6e4593c774aa255349919924cedffa841f7b66fb
Barrett Ross <barrettross23@gmail.com>
2021-04-15 12:34:58 -0400

    Making more upgrades

---
+++ Assets/Scripts/PickUps/AttackSpeedPickup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class AttackSpeedPickup : MonoBehaviour
+{
+    Pickup m_Pickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    // Update is called once per frame
+    void OnPicked(PlayerCharacterController player)
+    {
+        PlayerWeaponsManager weaponManager = player.GetComponent<PlayerWeaponsManager>();
+        WeaponController currentWeapon = weaponManager.GetActiveWeapon();
+        if (currentWeapon)
+        {
+            currentWeapon.delayBetweenShots *= 0.9f;
+
+            m_Pickup.PlayPickupFeedback();
+
+            Destroy(gameObject);
+        }
+    }
+}
