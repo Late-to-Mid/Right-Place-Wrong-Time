1a8d49be11c2e9430007366dc8b66c918634268a
Barrett Ross <barrettross23@gmail.com>
2021-04-17 12:19:56 -0400

    Adding area damage to grenade

--- Assets/Scripts/Weapons/Grenade.cs
+++ Assets/Scripts/Weapons/Grenade.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(DamageArea))]
 public class Grenade : MonoBehaviour
 {
+    [Header("Grenade Attributes")]
+    [Tooltip("Time until explosion after throwing")]
     public float lifetime = 3f;
-
+    [Tooltip("Radius of explosion")]
     public float explosionRadius = 5f;
-
+    [Tooltip("Force of explosion (for knockback)")]
     public float explosionForce = 100f;
+    [Tooltip("Damage value for actors")]
+    public float damage = 15f;
 
+    [Header("References")]
+    [Tooltip("Layers this projectile can collide with")]
+    public LayerMask hittableLayers = -1;
+    [Tooltip("Visual effects for explosion")]
     public GameObject explosionEffect;
+    [Tooltip("Damage Area Script")]
+    public DamageArea areaOfDamage;
 
     float m_SpawnTime;
+    const QueryTriggerInteraction k_TriggerInteraction = QueryTriggerInteraction.Collide;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +43,8 @@ public class Grenade : MonoBehaviour
     {
         Instantiate(explosionEffect, transform.position, transform.rotation);
 
+        areaOfDamage.InflictDamageInArea(damage, transform.position, hittableLayers, k_TriggerInteraction, gameObject);
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
 
         foreach (Collider nearbyObject in colliders)
@@ -43,11 +55,6 @@ public class Grenade : MonoBehaviour
                 RB.AddExplosionForce(explosionForce, transform.position, explosionRadius);
             }
 
-            if (nearbyObject.tag == "Enemy")
-            {
-                Health enemyHealth = nearbyObject.GetComponent<Health>();
-                enemyHealth
-            }
         }
 
         Destroy(gameObject);
