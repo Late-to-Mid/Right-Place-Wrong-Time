8cacc28e96c0b215f8ac2cb62896e1a23289775a
Barrett Ross <barrettross23@gmail.com>
2021-04-20 13:59:34 -0400

    Trying to get character-ability to work

---
+++ Assets/Scripts/Actors/Player/CharacterAbility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Actor))]
+public class CharacterAbility : MonoBehaviour
+{
+    [Tooltip("Dummy to be placed that enemies will shoot at")]
+    public GameObject playerDummyObject;
+    [Tooltip("Time stealth lasts")]
+    [Range(0f, 10f)]
+    public float timeLength = 5f;
+    [Tooltip("Hitbox of player, to be deactivated on ability use")]
+    public GameObject playerHitbox;
+
+    float m_TimeActivated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHitbox.activeSelf == false && Time.time > m_TimeActivated + timeLength)
+        {
+            playerHitbox.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) && playerHitbox.activeSelf)
+        {
+            playerHitbox.SetActive(false);
+            m_TimeActivated = Time.time;
+        }
+    }
+}
