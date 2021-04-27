75cfb25de2a76df3fefe3b95ccac157bd5aaddf0
Rossbarr <Rossbarr@msu.edu>
2021-04-27 10:52:04 -0400

    Revert "Merge branch 'master' into AbilityParticles"
    This reverts commit 144ea86524741935837f683f872c90684277c324, reversing
    changes made to d46789efbcae87a9407d21a9156da7ac34ee8687.

--- Assets/Scripts/UI/PlayerAbilityBar.cs
+++ Assets/Scripts/UI/PlayerAbilityBar.cs
@@ -1,18 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(FillBarColorChange))]
 public class PlayerAbilityBar : MonoBehaviour
 {
     [Tooltip("Image component displaying current ability")]
     public Image abilityFillImage;
-
-    [Header("Feedback")]
-    [Tooltip("Component to animate the color when empty or full")]
-    public FillBarColorChange FillBarColorChange;
-    [Tooltip("Sharpness for the fill ratio movements")]
-    public float barFillMovementSharpness = 20f;
-
 
     CharacterAbility ability;
 
@@ -21,15 +13,11 @@ public class PlayerAbilityBar : MonoBehaviour
         PlayerCharacterController playerCharacterController = GameObject.FindObjectOfType<PlayerCharacterController>();
 
         ability = playerCharacterController.GetComponent<CharacterAbility>();
-
-        FillBarColorChange.Initialize(1f, 0);
     }
 
     void Update()
     {
         // update ability bar value
-        float currentFillRatio = ability.readyBar;
-        abilityFillImage.fillAmount = Mathf.Lerp(abilityFillImage.fillAmount, currentFillRatio, Time.deltaTime * barFillMovementSharpness);
-        FillBarColorChange.UpdateVisual(currentFillRatio);
+        abilityFillImage.fillAmount = ability.readyBar;
     }
 }
