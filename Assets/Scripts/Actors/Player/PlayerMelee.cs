c5fe9bd4b128fb1d899b662e1af74f0a63f9ecce
Barrett Ross <barrettross23@gmail.com>
2021-06-14 17:27:11 -0400

    Start of melee

---
+++ Assets/Scripts/Actors/Player/PlayerMelee.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMelee : MonoBehaviour
+{
+    public GameObject weapon;
+    public LayerMask hittableLayers = -1;
+
+    Health m_Health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Health = GetComponent<Health>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Melee(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && Cursor.lockState == CursorLockMode.Locked)
+        {
+            weapon.transform.Translate(0f, 0f, 0.5f, weapon.transform);
+            Collider[] colliders = Physics.OverlapBox(transform.position + transform.forward * 0.5f, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.identity, hittableLayers, QueryTriggerInteraction.Ignore);
+
+        }
+    }
+}
