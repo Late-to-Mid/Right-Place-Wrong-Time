d9d74b4f8030a1ec131d7975c43e3e3a67520c6e
Barrett Ross <barrettross23@gmail.com>
2021-07-24 13:43:49 -0400

    Screen flash on damage taken

--- Assets/Scripts/UI/PlayerHealthBar.cs
+++ Assets/Scripts/UI/PlayerHealthBar.cs
@@ -7,18 +7,36 @@ public class PlayerHealthBar : MonoBehaviour
     [Tooltip("Image component dispplaying current health")]
     public Image healthFillImage;
 
+    float m_previousHealth;
+
     Health m_PlayerHealth;
+
+    public Image DamageIndicator;
 
     private void Start()
     {
         PlayerCharacterController playerCharacterController = GameObject.FindObjectOfType<PlayerCharacterController>();
 
         m_PlayerHealth = playerCharacterController.GetComponent<Health>();
+
+        m_previousHealth = m_PlayerHealth.currentHealth;
     }
 
     void Update()
     {
         // update health bar value
         healthFillImage.fillAmount = m_PlayerHealth.currentHealth / m_PlayerHealth.maxHealth;
+
+        if (m_PlayerHealth.currentHealth != m_previousHealth)
+        {
+            // Notify
+            DamageIndicator.gameObject.SetActive(true);
+
+            m_previousHealth = m_PlayerHealth.currentHealth;
+        }
+        else
+        {
+            DamageIndicator.gameObject.SetActive(false);
+        }
     }
 }
